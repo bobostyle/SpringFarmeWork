53188dbc384da885c1a2e95a6c6888255f727496
chenbiaobiao <118328036@qq.com>
2017-06-09 00:28:38 +0800

    添加两个接口，获取指定包名下的使用了指定注解的所有类。 获取指定包名下实现了某个接口的所有类。

--- src/main/java/org/smart4j/chapter1/util/ReflectionUtil.java
+++ src/main/java/org/smart4j/chapter1/util/ReflectionUtil.java
@@ -1,7 +1,10 @@
 package org.smart4j.chapter1.util;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * ReflectionUtil.java
@@ -53,4 +56,34 @@ public class ReflectionUtil {
 			throw new RuntimeException(e);
 		}
 	}
-}
+	
+	//传入包名和类对象，就可以获取该包下，使用了传入注解的所有类
+	//调用ClassUtil类的接口，获取指定包下的所有类
+	//Class.isAnnotationPresent(Class))
+	public static Set<Class<?>> getAllAnnotation(String packageName, Class<? extends Annotation> clazz){
+		Set<Class<?>> allClass = ClassUtil.getClassSet(packageName); 
+		Set<Class<?>> classes = new HashSet<Class<?>>();
+		for(Class<?> czz : allClass){
+			if(czz.isAnnotationPresent(clazz)){
+				classes.add(czz);
+			}
+		}
+		return classes;
+	}
+	
+	//获取指定包下，所有实现了某接口的类
+	//调用ClassUtil类的接口，获取指定包下的所有类
+	//public native boolean isAssignableFrom(Class<?> cls);
+	public static Set<Class<?>> getAllImplClass(String packageName, Class<?> clazz){
+		Set<Class<?>> clazzs = new HashSet<Class<?>>();
+		Set<Class<?>> allClass = ClassUtil.getClassSet(packageName); 
+		for(Class<?> clzz : allClass){
+			if(clazz.isAssignableFrom(clzz)){
+				clazzs.add(clzz);
+			}
+		}
+		return clazzs;
+	}
+}	
+
+
